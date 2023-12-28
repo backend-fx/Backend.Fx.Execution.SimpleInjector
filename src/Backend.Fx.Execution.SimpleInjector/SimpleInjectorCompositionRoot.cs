e55c0604fefe1e23f4d7cb33b26d8041f21a3d2b
Marc Wittke <marc@wittke-web.de>
2023-12-28 18:14:26 +0100

    fix deps

--- src/Backend.Fx.Execution.SimpleInjector/SimpleInjectorCompositionRoot.cs
+++ src/Backend.Fx.Execution.SimpleInjector/SimpleInjectorCompositionRoot.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Backend.Fx.DependencyInjection;
+using Backend.Fx.Execution.DependencyInjection;
 using Backend.Fx.Logging;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
